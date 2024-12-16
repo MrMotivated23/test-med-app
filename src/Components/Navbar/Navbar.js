81dc4a98f2e50c56685d1d2462ae0cb116c6d5e9
Dan <dww081000@gmail.com>
2024-12-15 23:29:11 -0500

    Added logic for ex 3

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,6 +7,27 @@ const Navbar = () => {
     const handleClick = () => setClick(!click);
     const [click, setClick] = useState(false);
 
+    const handleLogout = () => {
+        sessionStorage.removeItem("auth-token");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("phone");
+        // remove email phone
+        localStorage.removeItem("doctorData");
+        setIsLoggedIn(false);
+        // setUsername("");
+       
+        // Remove the reviewFormData from local storage
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          if (key.startsWith("reviewFormData_")) {
+            localStorage.removeItem(key);
+          }
+        }
+        setEmail('');
+        window.location.reload();
+    }
+
 return (
    
 <div>
@@ -61,6 +82,17 @@ return (
               <button className="btn1">Login</button>
             </a>
           </li>
+          {isLoggedIn?(
+          <>
+            <li className="link">
+              <button className="btn2" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+            
+          </>
+        ) : (
+          <>
         </ul>
     </nav>
 </div>
